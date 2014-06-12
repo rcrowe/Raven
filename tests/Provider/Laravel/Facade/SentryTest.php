6d9e8e6738250614d5fa6f3879396c7c8ff96334
Rob Crowe <hello@vivalacrowe.com>
2014-06-12 11:40:43 +0100

    Sentry facade tests

--- tests/Provider/Laravel/Facade/SentryTest.php
+++ tests/Provider/Laravel/Facade/SentryTest.php
@@ -5,6 +5,15 @@ namespace rcrowe\Raven\Tests\Provider\Laravel\Facade;
 use PHPUnit_Framework_TestCase;
 use Mockery as m;
 
+use Illuminate\Foundation\Application;
+use Illuminate\Config\Repository;
+use Illuminate\Queue\QueueManager;
+use rcrowe\Raven\Provider\Laravel\Log;
+use Monolog\Logger;
+use rcrowe\Raven\Provider\Laravel\RavenServiceProvider;
+
+use rcrowe\Raven\Provider\Laravel\Facade\Sentry;
+
 class SentryTest extends PHPUnit_Framework_TestCase
 {
     public function tearDown()
@@ -12,9 +21,65 @@ class SentryTest extends PHPUnit_Framework_TestCase
         m::close();
     }
 
-    public function testTemp()
+    public function testFacadeInstance()
     {
+        $this->bootApplication();
 
+        $this->assertInstanceOf('rcrowe\Raven\Client', Sentry::getFacadeRoot());
+    }
+
+    public function testSetRemoveUser()
+    {
+        $app  = $this->bootApplication();
+        $user = array(
+            'id'   => 1,
+            'name' => 'Rob Crowe',
+        );
+
+        Sentry::setUser($user);
+        $this->assertEquals($app['log.raven']->context->user, $user);
+
+        Sentry::removeUser();
+        $this->assertEquals($app['log.raven']->context->user, array());
+    }
+
+    protected function bootApplication($enabled = true)
+    {
+        $app          = new Application;
+        $app['env']   = 'production';
+        $app['queue'] = new QueueManager($app);
+
+        $config = new Repository(m::mock('Illuminate\Config\LoaderInterface'), 'production');
+
+        $config->getLoader()->shouldReceive('addNamespace');
+        $config->getLoader()->shouldReceive('cascadePackage')
+            ->andReturnUsing(function ($env, $package, $group, $items) {
+                return $items;
+            });
+        $config->getLoader()->shouldReceive('exists')->with('environments', 'raven')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('dsn', 'raven')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('enabled', 'raven')->andReturn(false);
+        $config->getLoader()->shouldReceive('exists')->with('level', 'raven')->andReturn(false);
+        $config->getLoader()->shouldReceive('load')->with('production', 'config', 'raven')->andReturn(
+            array(
+                'dsn'     => 'http://123:456@foo.com/789',
+                'enabled' => $enabled,
+                'level'   => 'critical',
+            )
+        );
+        $app['config'] = $config;
+
+        $logger = new Log(new Logger('test'));
+        $app['log'] = $logger;
+
+
+        $provider = new RavenServiceProvider($app);
+        $provider->register();
+        $provider->boot();
+
+        Sentry::setFacadeApplication($app);
+
+        return $app;
     }
 
     // public function testGetSentry()
