514793c14cfb247e05c639aa6680ac38097fcf0d
Rob Crowe <hello@vivalacrowe.com>
2014-06-13 11:41:36 +0100

    Better alignment

--- tests/Provider/Laravel/ProviderTest.php
+++ tests/Provider/Laravel/ProviderTest.php
@@ -17,12 +17,12 @@ class ProviderTest extends Base
 
         $config = m::mock('Illuminate\Config\Repository');
         $config->shouldReceive('package')
-                ->once()
-                ->with(
-                    'rcrowe/raven',
-                    realpath(__DIR__.'/../../../src/config'),
-                    'raven'
-                );
+               ->once()
+               ->with(
+                   'rcrowe/raven',
+                   realpath(__DIR__.'/../../../src/config'),
+                   'raven'
+               );
         $app['config'] = $config;
 
         (new RavenServiceProvider($app))->register();
